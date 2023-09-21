46d659980e4a37ed38f26872454130c30ed7cf41
amanmulani09 <mulaniaman0504@gmail.com>
2023-09-22 00:40:11 +0530

    worked on header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,8 +1,26 @@
-
+import Styles from './Header.module.css'
 const Header = () => {
   return (
-    <header>
+    <header className={Styles.header}>
 
+      <div>
+        <div>
+          <h3 className={Styles.header_main_logo}>Job<span>Sync</span></h3>
+        </div>
+      </div>
+
+      <div>
+        <ul className={Styles.header_main_links}>
+          <li>Find Jobs</li>
+          <li>Find Company</li>
+          <li>Find Resources</li>
+        </ul>
+      </div>
+
+      <div className={Styles.header_main_buttons}>
+       <button>SignIn</button>
+       <button>SIgnUp</button>
+      </div>
     </header>
   )
 }
